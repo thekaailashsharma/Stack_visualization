f3810bdcb6e43d7d7b4e379b04fd147db66da799
Kailash <kailashps.1011@gmail.com>
2022-08-21 20:37:43 +0530

    Stack Visualization using Javafx Developer Kailash Sharma

--- src/main/java/com/stk/stk/HelloApplication.java
+++ src/main/java/com/stk/stk/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 764, 446);
-        Scene scene = new Scene(fxmlLoader.load(), 800, 465);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("log.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 764, 446);
+//        Scene scene = new Scene(fxmlLoader.load(), 800, 465);
         stage.setTitle("Data Structure Visualization!");
         stage.setResizable(false);
         stage.setScene(scene);
